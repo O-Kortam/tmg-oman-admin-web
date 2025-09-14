34831b39dcf01e7b9d156ea22f0707921f44cc62
O-Kortam <95567076+O-Kortam@users.noreply.github.com>
2025-09-14 16:37:12 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -7,12 +7,17 @@ const nextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'tmg-oman-strapi.tmg-service-eu-de-1-cx2-1-24c93556c7052421b1e777d2c03a04e1-0000.eu-de.containers.appdomain.cloud',
+        hostname:
+          'tmg-oman-strapi.tmg-service-eu-de-1-cx2-1-24c93556c7052421b1e777d2c03a04e1-0000.eu-de.containers.appdomain.cloud',
       },
     ],
   },
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  env: {
+    NEXT_PUBLIC_API_URL:
+      'https://tmg-oman-strapi.tmg-service-eu-de-1-cx2-1-24c93556c7052421b1e777d2c03a04e1-0000.eu-de.containers.appdomain.cloud',
   },
 };
 
